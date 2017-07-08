4983fa365c8299450f571f37162da154730a10ad
Kristine Morinaka <kfmorinaka@gmail.com>
2017-07-08 16:12:44 -0700

    Adds empty templates Sets up routes

--- server.py
+++ server.py
@@ -21,7 +21,38 @@ def index():
 
     return render_template("homepage.html")
 
+@app.route('/new', methods=['GET'])
+def new():
+    # include languages
+    return render_template("new.html")
 
+@app.route('/volunteer/', methods=['POST'])
+def create():
+    # create new volunteer with form params
+    # then redirect profile page
+    # pass id
+    id = 1
+    return redirect("/volunteer/%s" % id)
+
+@app.route('/volunteer/:id', methods=['GET'])
+def show():
+    # query with id
+    # show volunteer info
+
+    return render_template("profile.html")
+
+@app.route('/volunteer/:id/edit', methods=['GET'])
+def edit():
+    #  show info in form to be edited
+
+    return render_template("edit.html")
+
+@app.route('/volunteer/:id', methods=['PUT'])
+def update():
+    # update with new params
+
+    id = 1
+    return redirect("/volunteer/%s" % id)
 
 if __name__ == "__main__":
   # We have to set debug=True here, since it has to be True at the point
