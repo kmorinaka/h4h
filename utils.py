7d8bbb7ccd4a5248f6d451057bc0b3d0218c9926
angelacastanieto <angelacastanieto@Angelas-MacBook-Pro.local>
2017-07-08 22:49:58 -0700

    lang detector

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import logging
+import os
+import detectlanguage
 from model import connect_to_db, db, Language, Volunteer
 
 TWILIO_NUMBER = "+16787374363"
+detectlanguage.configuration.api_key = os.environ.get("DETECTLANGUAGE_API_KEY")
 
 def get_volunteer_numbers():
     volunteers = Volunteer.query.all()
@@ -17,3 +20,15 @@ def phone_number_formatter(phone_number):
     if len(phone_number) == 10:
         return "+1{}".format(phone_number)
     logging.error("invalid phone number in database: {}".format(phone_number))
+
+def detect_language(text):
+    """detects the language a text string is in and returns the language as a string"""
+    langs = {
+      "en": "English",
+      "sp": "Spanish",
+      "fr": "French"
+    }
+
+    lang_code = detectlanguage.simple_detect(text)
+
+    return langs[lang_code]
