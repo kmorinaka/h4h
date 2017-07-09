a667b5507eeec09375fc95dc604204c3425926b8
Kristine Morinaka <kfmorinaka@gmail.com>
2017-07-08 23:56:40 -0700

    Adds volunteers page

--- server.py
+++ server.py
@@ -93,10 +93,17 @@ def show(id):
 @app.route('/volunteer/<id>/edit', methods=['GET'])
 def edit(id):
     """" Show a prefilled volunteer form for editing """
-
+    languages = Language.query.all()
     volunteer = Volunteer.query.get(id)
 
-    return render_template("edit.html")
+    return render_template("edit.html", volunteer=volunteer,languages=languages)
+
+@app.route('/volunteers/all', methods=['GET'])
+def volunteers():
+
+  volunteers = Volunteer.query.all()
+
+  return render_template("volunteers.html", volunteers=volunteers)
 
 
 @app.route('/volunteer/<id>', methods=['PUT'])
